f38d4f03c85b73f253e8a8c9babf36b60a26bdf2
mygit@23 <naveenmadhesan23@gmail.com>
2024-01-02 14:39:32 +0530

    Add project files.

---
+++ Jan2Console/PartialClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jan2Console
+{
+    public class PartialClass
+    {//not accessable inother acces modifier
+        //static void Main(string[] args)
+        //{
+        //    Dev dev = new Dev();
+        //    dev.Display();
+        //    dev.Display2();
+        //    Console.ReadLine();
+        //}
+    }
+    partial class Dev
+    {
+        public void Display() {
+            Console.WriteLine("Developer 1 Display Method");
+        }
+    }
+    partial class Dev
+    {
+        public void Display2() { Console.WriteLine("Developer 2 Display Method"); } //method shoul not be repeated
+    }
+    partial class Dev
+    {
+        static void Main(string[] args)
+        {
+            Dev dev = new Dev();
+            dev.Display();
+            dev.Display2();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Jan2Console/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jan2Console
+{
+    sealed class ProgramClass
+    {
+        public ProgramClass() { }
+        private ProgramClass(int a) { }
+        static ProgramClass()
+        {
+            Console.WriteLine("static class constructor");
+        }
+        protected ProgramClass(string name) { }
+        public void display()
+        {
+            Console.WriteLine("sealed public method");
+        }
+        protected void display2()
+        {
+            Console.WriteLine("sealed protected method");
+        }
+        private void display3()
+        {
+            Console.WriteLine("sealed private method");
+        }
+    }
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            ProgramClass programClass = new ProgramClass();
+            programClass.display();
+            // programClass.display2);//protected methods are not accesable
+         Console.ReadLine();    
+        }
+
+        //public class program2:ProgramClass { } //cant derived from sealed class inheritance not posible
+    }
+}
